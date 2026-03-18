615d8b4f6e150dcc8d07fc740c992851abbca501
Vridhi <rajeevvridhi@gmail.com>
2026-03-18 09:18:38 +0530

    Add files via upload

---
+++ class_work/arithmeticmodule.py
@@ -0,0 +1,23 @@
+def calculateSum(x,y):
+    return(x+y)
+
+def calculateSub(x,y):
+    return(x-y)
+
+def calculateMultiply(x,y):
+    return(x*y)
+
+def calculateDivision(x,y):
+    return(x/y)
+
+def rect_area(length, width):
+    return length * width
+
+def rect_perimeter(length, width):
+    return 2 * (length + width)
+
+def square_area(side):
+    return side * side
+
+def square_perimeter(side):
+    return 4 * side
